31da835b47cdb8503ed3e5535ba29b8354ef08a6
Lunargim <loryluna2001@gmail.com>
2025-08-29 16:37:49 +0200

    Feat: now fishes spawn

--- Assets/Scripts/FishSpawner/FishSpawner.cs
+++ Assets/Scripts/FishSpawner/FishSpawner.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
 public class FishSpawner : MonoBehaviour
 {
+    [Header("RangeTimer")]
+    public float minTimeToWait;
+    public float maxTimeToWait;
+
     [SerializeField] private GameObject[] _fishesToSpawn;
 
     private Collider2D _spawnerCollider;
     public void Awake()
     {
         _spawnerCollider = GetComponent<Collider2D>();
-
     }
+
     public void Start()
     {
-        FishSpawnerManager.instance.SpawnFish(_spawnerCollider, _fishesToSpawn);
+        StartCoroutine("SpawnFishes");
+    }
+
+    IEnumerator SpawnFishes()
+    {
+        while (true)
+        {
+            FishSpawnerManager.instance.SpawnFish(_spawnerCollider, _fishesToSpawn);
+            yield return new WaitForSeconds(Random.Range(minTimeToWait, maxTimeToWait));
+        }
     }
 }
